029109ac9b0a07d6e829a0303265864df76ee2dd
802ScubaFish <christianrdow@gmail.com>
2020-06-05 15:39:19 -0400

    Fixed 'Unrec.Input' bug in 'comuterGuesses'

--- index.js
+++ index.js
@@ -20,10 +20,9 @@ function midValue(min, max) {
 // Start Game Function
 start()
 async function start() {
-  console.log('Welcome to my Guessing Game!')
-  let gameChoice = await ask('To Start Please Choose either Computer Guesses or Human Guesses\n')
+  console.log('Welcome to my Guessing Game!\n' + '*Games alternate after intital selection*\n')
+  let gameChoice = await ask('To Start Please Choose Either Computer Guesses or Human Guesses\n')
   gameChoice = gameChoice.trim().toLowerCase()
-  //console.log('You Have Chosen ' + gameChoice + '\n')
   if (gameChoice == 'computer guesses') {
     computerGuesses()
   }
@@ -38,30 +37,28 @@ async function computerGuesses() {
   let userMaxRange = parseInt(await ask('What is your desired top end range?\n'));
   console.log('You entered: ' + userMinRange + ' to ' + userMaxRange)
   let computerGenNum = randomNum(userMinRange, userMaxRange)
-  console.log("Please choose your secret number within your specifed range now and remember it")
+  console.log("Please choose a secret number within your chosen range now and remember it")
   let answer = await ask('Is your number ' + computerGenNum + ' (yes / no)' + '?\n')
-  
+
   while (answer !== 'yes') {
-    let highLow = await ask('Is your number lower or higher or correct?\n'); {
-      if (highLow.toLowerCase() === 'higher') {
-        userMinRange = computerGenNum + 1
-        computerGenNum = midValue(userMinRange, userMaxRange)
-        console.log('My next guess is ' + computerGenNum + '\n')
-      } else if (highLow.toLowerCase() === 'lower') {
-        userMaxRange = computerGenNum - 1
-        computerGenNum = midValue(userMinRange, userMaxRange)
-        console.log('My next guess is ' + computerGenNum + '\n')
-      } else if (highLow.toLowerCase() === 'correct') {
-        console.log('I have Won!\n' + 'Skynet Activated')
-        humanGuesses()
-      } else {
-        console.log('Unrecognized input, please start over')
-        process.ext()
-      }
-      if (userMaxRange < userMinRange) {
-        console.log('Cheater! Start Over')
-        process.exit()
-      }
+    let highLow = await ask('Is your number lower or higher or correct?\n');
+    highLow = highLow.trim().toLowerCase()
+    if (highLow === 'higher') {
+      userMinRange = computerGenNum + 1
+      computerGenNum = midValue(userMinRange, userMaxRange)
+      console.log('My next guess is ' + computerGenNum + '\n')
+    } else if (highLow === 'lower') {
+      userMaxRange = computerGenNum - 1
+      computerGenNum = midValue(userMinRange, userMaxRange)
+      console.log('My next guess is ' + computerGenNum + '\n')
+    } else if (highLow === 'correct') {
+      console.log('I have Won!\n' + 'Skynet Activated')
+      humanGuesses()
+    } else {
+      console.log('Unrecognized input, Try Again')
+    } if (userMaxRange < userMinRange) {
+      console.log('Cheater! Start Over')
+      process.exit()
     }
   } if (answer.toLowerCase() === 'yes') {
     console.log('I have Won!\n' + 'Skynet Activated')
@@ -75,18 +72,19 @@ async function humanGuesses() {
   let userMaxRange = parseInt(await ask('What is your desired top end range?\n'));
   console.log('You entered: ' + userMinRange + ' to ' + userMaxRange)
   let computerGenNum = randomNum(userMinRange, userMaxRange)
-  console.log("I have chosen a secret number within that range for you to guess,")
+  console.log("I have chosen a secret number within your range for you to guess,")
 
   while (answer !== computerGenNum) {
-    let humanAnsw = await ask('Please make a guess\n'); {
-      if (humanAnsw < computerGenNum) {
-        console.log('My mumber is higher\n')
-      } else if (humanAnsw > computerGenNum) {
-        console.log('My number is lower\n')
-      }
-    } if (humanAnsw == computerGenNum) {
+    let humanAnsw = await ask('Please make a guess\n');
+    if (humanAnsw < computerGenNum) {
+      console.log('My mumber is higher\n')
+    } else if (humanAnsw > computerGenNum) {
+      console.log('My number is lower\n')
+    } else if (humanAnsw == computerGenNum) {
       console.log('You have Won!' + '\n' + 'Computer Termnated\n' + 'Skynet Destoryed\n')
       computerGuesses()
+    } else {
+      console.log('Unrecognized input, Try Again')
     }
   }
 }
